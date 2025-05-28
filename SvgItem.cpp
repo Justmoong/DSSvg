075b0c70d2c83e07d7b054e488fc2c5db3e0ec15
Justmoong <andudyun0504@gmail.com>
2025-05-28 15:02:01 +0900

    Add API Plugin

---
+++ SvgItem.cpp
@@ -0,0 +1,27 @@
+//
+// Created by Justmoong on 5/28/25.
+//
+#pragma once
+
+#include <QQuickPaintedItem>
+#include <QSvgRenderer>
+#include <QPainter>
+#include <QString>
+#include <QColor>
+#include <QtQml/qqml.h>
+
+class SvgItem : public QQuickPaintedItem {
+    Q_OBJECT
+    Q_PROPERTY(QString source READ source WRITE setSource NOTIFY sourceChanged)
+    Q_PROPERTY(QString elementId READ elementId WRITE setElementId NOTIFY elementIdChanged)
+    Q_PROPERTY(QColor colorOverride READ colorOverride WRITE setColorOverride NOTIFY colorOverrideChanged)
+
+public:
+    void paint(QPainter *painter) override;
+
+private:
+    QString m_source;
+    QString m_elementId;
+    QColor m_colorOverride;
+    QSvgRenderer m_renderer;
+};
